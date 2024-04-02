deac6055cbb04d81358835af5ce6face98e03d1b
tduyng <@project_362_bot_62c1427c07ac651e3aa5ccd8bc68d4ae@noreply.gitlab.canalplus.pro>
2024-04-02 18:01:51 +0200

    feat: decode bencoded string

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use serde_json;
 use std::env;
-
-// Available if you need it!
 // use serde_bencode
 
 #[allow(dead_code)]
@@ -19,19 +17,14 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     }
 }
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
