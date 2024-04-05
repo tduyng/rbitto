826b166c34ba219af1c1e2fbd362f61197e58043
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-05 18:09:06 +0200

    fix: handle the nested list

--- src/utils.rs
+++ src/utils.rs
@@ -21,6 +21,7 @@ pub fn decode(encoded_value: &str) -> Value {
 fn decode_inner(encoded_value: &str) -> (Value, &str) {
     let regex_digit = Regex::new(r"^(\d+):(.+)$").unwrap();
     let regex_integer = Regex::new(r"^i(-?\d+)e").unwrap();
+    let regex_list = Regex::new(r"^l(.+)e").unwrap();
 
     match encoded_value {
         encoded if regex_digit.is_match(encoded) => {
@@ -39,6 +40,22 @@ fn decode_inner(encoded_value: &str) -> (Value, &str) {
             (
                 Value::Number(Number::from(parsed_number)),
                 &encoded[number_end + 1..],
+            )
+        }
+        encoded if regex_list.is_match(encoded) => {
+            let captures = regex_list.captures(encoded).unwrap();
+            let inner_value = &captures[1];
+            let mut decoded_list = Vec::new();
+            let mut remaining = inner_value;
+
+            while !remaining.is_empty() {
+                let (decoded_element, new_remaining) = decode_inner(remaining);
+                decoded_list.push(decoded_element);
+                remaining = new_remaining;
+            }
+            (
+                Value::Array(decoded_list),
+                &encoded[encoded.len() - remaining.len()..],
             )
         }
         _ => panic!("Unhandled encoded value: {}", encoded_value),
