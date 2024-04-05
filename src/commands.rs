a170e3ecc2a0097416da236e8a7e56d413b1e47d
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-05 18:22:04 +0200

    fix: use serde_bencode

--- src/commands.rs
+++ src/commands.rs
@@ -5,7 +5,7 @@ pub struct Commands {}
 
 impl Commands {
     pub fn decode(data: &str) -> Result<()> {
-        let decoded_value = utils::decode(data);
+        let decoded_value = utils::decode(data)?;
 
         println!("{}", decoded_value);
         Ok(())
--- src/utils.rs
+++ src/utils.rs
@@ -1,63 +1,24 @@
-use regex::Regex;
+use anyhow::Result;
 use serde_json::{Number, Value};
 
-pub fn decode(encoded_value: &str) -> Value {
-    if encoded_value.starts_with('l') && encoded_value.ends_with('e') {
-        let inner_value = &encoded_value[1..encoded_value.len() - 1];
-        let mut decoded_list = Vec::new();
-        let mut remaining = inner_value;
-
-        while !remaining.is_empty() {
-            let (decoded_element, new_remaining) = decode_inner(remaining);
-            decoded_list.push(decoded_element);
-            remaining = new_remaining;
-        }
-        Value::Array(decoded_list)
-    } else {
-        decode_inner(encoded_value).0
-    }
+pub fn decode(encoded_value: &str) -> Result<Value> {
+    let value: serde_bencode::value::Value = serde_bencode::from_str(encoded_value)?;
+    decode_inner(value)
 }
 
-fn decode_inner(encoded_value: &str) -> (Value, &str) {
-    let regex_digit = Regex::new(r"^(\d+):(.+)$").unwrap();
-    let regex_integer = Regex::new(r"^i(-?\d+)e").unwrap();
-    let regex_list = Regex::new(r"^l(.+)e$").unwrap();
-
+fn decode_inner(encoded_value: serde_bencode::value::Value) -> Result<Value> {
     match encoded_value {
-        encoded if regex_digit.is_match(encoded) => {
-            let length_end = encoded.find(':').unwrap();
-            let length: usize = encoded[..length_end].parse().unwrap();
-            let string = &encoded[length_end + 1..length_end + 1 + length];
-            (
-                Value::String(string.to_string()),
-                &encoded[length_end + 1 + length..],
-            )
+        serde_bencode::value::Value::Bytes(b) => Ok(Value::String(String::from_utf8(b)?)),
+        serde_bencode::value::Value::Int(i) => Ok(Value::Number(Number::from(i))),
+        serde_bencode::value::Value::List(l) => {
+            let array = l
+                .into_iter()
+                .map(decode_inner)
+                .collect::<anyhow::Result<Vec<serde_json::Value>>>()?;
+            Ok(Value::Array(array))
         }
-        encoded if regex_integer.is_match(encoded) => {
-            let number_end = encoded.find('e').unwrap();
-            let number = &encoded[1..number_end];
-            let parsed_number = number.parse::<i64>().unwrap();
-            (
-                Value::Number(Number::from(parsed_number)),
-                &encoded[number_end + 1..],
-            )
+        _ => {
+            panic!("Unhandled encoded value: {:?}", encoded_value)
         }
-        encoded if regex_list.is_match(encoded) => {
-            let captures = regex_list.captures(encoded).unwrap();
-            let inner_value = &captures[1];
-            let mut decoded_list = Vec::new();
-            let mut remaining = inner_value;
-
-            while !remaining.is_empty() {
-                let (decoded_element, new_remaining) = decode_inner(remaining);
-                decoded_list.push(decoded_element);
-                remaining = new_remaining;
-            }
-            (
-                Value::Array(decoded_list),
-                &encoded[encoded.len() - remaining.len()..],
-            )
-        }
-        _ => panic!("Unhandled encoded value: {}", encoded_value),
     }
 }
