d6449f50edc25bb944cdb30a576aa80d433d3e8b
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-07 09:43:13 +0200

    fix: add compare hashes

--- src/commands.rs
+++ src/commands.rs
@@ -1,11 +1,10 @@
 use std::fs;
-
 use crate::{
     torrent::{HandShake, Stream, Torrent, Tracker, HANDSHAKE_BUF_INDEX_START},
     utils,
 };
 use anyhow::{Context, Ok, Result};
-// use sha1::{Digest, Sha1};
+use sha1::{Digest, Sha1};
 
 pub struct Commands {}
 
@@ -74,19 +73,19 @@ impl Commands {
         println!("Debug: await_unchoke excuted!");
 
         let piece_data: Vec<u8> = stream.get_piece_data(piece_index, &torrent).await?;
-        // let mut hasher = <Sha1 as Digest>::new();
-        // hasher.update(&piece_data);
-        // let piece_hash: [u8; 20] = hasher.finalize().into();
+        let mut hasher = <Sha1 as Digest>::new();
+        hasher.update(&piece_data);
+        let piece_hash: [u8; 20] = hasher.finalize().into();
 
-        // let torrent_hashes = &torrent.info.pieces[..];
-        // let piece_hash_index = (piece_index * 20) as usize;
+        let torrent_hashes = &torrent.info.pieces[..];
+        let piece_hash_index = (piece_index * 20) as usize;
 
-        // // Extract the relevant portion of the torrent hash
-        // let mut torrent_hash: [u8; 20] = [0; 20];
-        // torrent_hash.copy_from_slice(&torrent_hashes[piece_hash_index..piece_hash_index + 20]);
-        // if piece_hash != torrent_hash {
-        //     panic!("Hashes do NOT match!");
-        // }
+        // Extract the relevant portion of the torrent hash
+        let mut torrent_hash: [u8; 20] = [0; 20];
+        torrent_hash.copy_from_slice(&torrent_hashes[piece_hash_index..piece_hash_index + 20]);
+        if piece_hash != torrent_hash {
+            panic!("Hashes do NOT match!");
+        }
 
         fs::write(output, piece_data).context("Ctx: write piece data failed!")?;
 
